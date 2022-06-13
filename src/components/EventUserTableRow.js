3309e939e9b37ece094bb86bd0160d321bb56166
Aaron Flood <aaronjflood@gmail.com>
2022-06-13 09:14:32 -0500

    Mapped room id to name in event list.

--- src/components/EventUserTableRow.js
+++ src/components/EventUserTableRow.js
@@ -4,29 +4,33 @@ import axios from 'axios'
 import Button from 'react-bootstrap/Button'
 
 export default class RoomTableRow extends Component {
-  constructor(props) {
-    super(props)
-    this.deleteRoom = this.deleteRoom.bind(this)
-  }
 
-  deleteRoom() {
-    axios
-      .delete(
-        'http://localhost:4000/room/delete/' + this.props.obj._id,
-      )
-      .then((res) => {
-        console.log('Room successfully deleted!')
+    constructor(props) {
+      super(props)
+      this.state = {
+        room_name: ""
+      };
+      console.log(this.props.user)
+    }
+
+  async componentWillMount() {
+    await axios.get('http://localhost:4000/room/find-id/' + this.props.obj.room)
+      .then(res => {
+        this.setState({
+          room_name: res.data.name
+        });
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       })
   }
+
 
   render() {
     return (
       <tr>
         <td>{this.props.obj.name}</td>
-        <td>{this.props.obj.room}</td>
+        <td>{this.state.room_name}</td>
         <td>{this.props.obj.approved.toString()}</td>
         {/* <td>
           <Link
