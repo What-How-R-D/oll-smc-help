a471731fa8320f19a824a0e57c254eba5de3cbd9
Aaron Flood <aaronjflood@gmail.com>
2022-07-25 16:10:03 -0500

    Updated BM table row to pull guests info.

--- src/components/EventBMTableRow.js
+++ src/components/EventBMTableRow.js
@@ -93,6 +93,21 @@ export default class RoomTableRow extends Component {
 
   }
 
+  get_name() {
+    if (this.props.obj.requester){ return <td>{this.state.user.name}</td>}
+    else { return <td>{this.props.obj.contact}</td>}
+  }
+
+  get_email(){
+    if (this.props.obj.requester){ return <td>{this.state.user.email}</td>}
+    else { return <td>{this.props.obj.email}</td>}
+  }
+
+  get_phone(){
+    if (this.props.obj.requester){ return <td>{this.state.user.phone}</td>}
+    else { return <td>{this.props.obj.phone}</td>}
+  }
+
   buttons() {
     if (this.props.obj.status === "Pending") {
       return  <td>
@@ -106,9 +121,9 @@ export default class RoomTableRow extends Component {
     return (
       <tr>
         <td>{this.props.obj.name}</td>
-        <td>{this.state.user.name}</td>
-        <td>{this.state.user.email}</td>
-        <td>{this.state.user.phone}</td>
+        {this.get_name()}
+        {this.get_email()}
+        {this.get_phone()}
         <td>{this.state.room_name}</td>
         <td>{format(new Date(this.props.obj.startTime), "M/d/yyyy h:mm a")}</td>
         <td>{format(new Date(this.props.obj.endTime), "M/d/yyyy h:mm a")}</td>
